972e37ce7bd0576600c097330f14bb0a49c4cc55
Arphor <gabrielpaiva18@gmail.com>
2021-06-24 23:09:21 -0300

    Equal finished

--- equal/include/equal.h
+++ equal/include/equal.h
@@ -16,8 +16,17 @@ namespace graal {
 template<class InputIt1, class InputIt2, class Equal>
 bool equal(InputIt1 first1, InputIt1 last1, InputIt2 first2, Equal eq)
 {
-    // TODO
-    return false;
+    
+    while(first1!=last1){
+        if(eq(*first1, *first2)==false){
+            return false;
+        }
+        first1++;
+        first2++;
+    }
+
+
+    return true;
 }
 
 template<class InputIt1, class InputIt2, class Equal>
@@ -25,8 +34,19 @@ bool equal(InputIt1 first1, InputIt1 last1,
            InputIt2 first2, InputIt2 last2,
            Equal eq )
 {
-    // TODO
-    return false;
+    if((first1-last1)!=(first2-last2)){
+        return false;
+    }
+    while(first1!=last1){
+        if(eq(*first1, *first2)==false){
+            return false;
+        }
+
+        first1++;
+        first2++;
+    }
+
+    return true;
 }
 
 }
