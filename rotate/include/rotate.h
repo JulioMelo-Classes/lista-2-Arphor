e5e6eca719d53756eb94d665607fc18c820a24f1
Arphor <gabrielpaiva18@gmail.com>
2021-06-27 19:39:55 -0300

    Rotate finished

--- rotate/include/rotate.h
+++ rotate/include/rotate.h
@@ -16,8 +16,23 @@ namespace graal {
 template<class ForwardIt>
 ForwardIt rotate(ForwardIt first, ForwardIt n_first, ForwardIt last)
 {
-    // TODO
-   return last;
+    int x = n_first - first;
+
+
+    while(n_first<last){
+        for(ForwardIt i=n_first; i>first; i--){
+            auto temp = *i;
+            *i = *(i-1);
+            *(i-1) = temp;
+        }
+        first++;
+        n_first++;
+    
+    }
+
+
+
+   return (last-x);
 }
 
 }
